0bb7939abe806a44d7c451e73831d70ed0c46a52
vlad <vlad.s.o@yandex.ru>
2022-04-27 17:54:34 +0300

    27_04_com_ev

--- main.c
+++ main.c
@@ -1,36 +1,53 @@
 #include "mcc_generated_files/mcc.h"
 
-
-
+//
+#define DEBUG
 
 //SETUP 
-#define _XTAL_FREQ 4000000
-#define ROTATION_DAYS 14
-#define LOW_WATER_RESISTANSE 20000
-#define HIGH_WATER_RESISTANSE 25000
-#define UP_RESISTANSE 20000
-#define LOW_PIN_VOLTAGE 2000
-#define RELE_TIME 10//120 sec
-#define RELE_GAP 100 //100 msec
-#define WSP_MEAS_COUNT 2
-#define FUN_MEAS_COUNT 10
-#define JUMP_MEAS_COUNT 10
+#define _XTAL_FREQ 4000000 //??????? ??
+#define ROTATION_DAYS 14 //???? ?? ???????? ?????
+#define LOW_WATER_RESISTANSE 20000  //????????????? ???????
+#define HIGH_WATER_RESISTANSE 25000 //
+#define UP_RESISTANSE 20000 //????????????? ????????
+#define LOW_PIN_VOLTAGE 2000 // "?????? ?????????? ???????"
 
-const long int BAD_WSP_VOLTAGE = 20000; //??? LOW_WATER_RESISTANSE/((UP_RESISTANSE+LOW_WATER_RESISTANSE)/256);	//	????????? ???????? ADC ??? ??????????? ??????? "????" ?? ???????
-const long int GOOD_WSP_VOLTAGE = 40000; //??? HIGH_WATER_RESISTANSE/((UP_RESISTANSE+HIGH_WATER_RESISTANSE)/256);//	????????? ???????? ADC ??? ??????????? ?????????? "????" ?? ???????
-const long int ROTATION_TIME = 60; //(ROTATION_DAYS * 24 * 60 * 60); //D*H*M*S
+//?????? ?? ????????
+#define WSP_MEAS_COUNT 2    //?????????? ????????? ???????
+#define FUN_MEAS_COUNT 10   //?????????? ????????? ?????????????
+#define JUMP_MEAS_COUNT 10  //?????????? ????????? ????????
+
+
+#ifdef DEBUG
+
+#define RELE_TIME 10// sec
+#define RELE_GAP 1 // sec
+const long int BAD_WSP_VOLTAGE = 20000; //
+const long int GOOD_WSP_VOLTAGE = 40000; //
+const long int ROTATION_TIME = 60; //sec
+
+#else
+
+#define RELE_TIME 120 // sec
+#define RELE_GAP 1 //sec
+const long int BAD_WSP_VOLTAGE = LOW_WATER_RESISTANSE / ((UP_RESISTANSE + LOW_WATER_RESISTANSE) / 256); //	TODO
+const long int GOOD_WSP_VOLTAGE = HIGH_WATER_RESISTANSE / ((UP_RESISTANSE + HIGH_WATER_RESISTANSE) / 256); //	TODO
+const long int ROTATION_TIME = (ROTATION_DAYS * 24 * 60 * 60); //D*H*M*S
+
+#endif
 
 //END SETUP
+
+//?????
 
 struct f_field {
     unsigned ALARM : 1;
     unsigned NORMAL_WORK_MODE : 1;
     unsigned CLOSED : 1;
-    unsigned FUN_CONNECTED : 1;
-    unsigned JUMP : 1;
-    unsigned CLOSING : 1;
+    unsigned _FUN_CONNECTED : 1;
+    unsigned _JUMP : 1;
     unsigned RELE_POW_WAIT : 1;
     unsigned RELE_CON_WAIT : 1;
+    unsigned FREE : 1;
 };
 
 union Byte {
@@ -38,80 +55,63 @@ union Byte {
     struct f_field bits;
 } FLAGS;
 
-unsigned char time_pow;
-static unsigned char time_con;
+unsigned char time_pow_s; //????? ?? ???????? ???? (???)
+long int time_s; //????? ?? ???????????? (???)
 
-static signed char fun_counter;
-static unsigned char measures;
-static signed char jump_counter;
-long int time_s;
-unsigned result;
-unsigned jresult;
-unsigned fresult;
-
-//served
-
-void switch_zum() {
+void switch_zum() {//???? ????????????
     PIN_ZUMMER_Toggle();
 }
 
-void toggle_tone() {
+void toggle_tone() {//???/???? ??????
     INTCONbits.TMR0IE = ~INTCONbits.TMR0IE;
 }
 
-void beep() {
-    for (char i = 0; i < 40; i++) {
-        switch_zum();
-        __delay_ms(1);
+void beep(unsigned delay, unsigned pause, char time, char count) {//???????? ????
+    for (char j = 0; j < count; j++) {
+        for (char i = 0; i < time; i++) {
+            switch_zum();
+            __delay_us(delay);
+        }
+        __delay_us(pause);
     }
-   
 }
 
-void boop() {
-    for (char i = 0; i < 50; i++) {
-        switch_zum();
-        __delay_us(300);
-    }
-   
-}
-//end served
-
-void go_close() {
+void go_close() {//?????? ???????? ??????
     time_s = 0;
     PIN_RELE_CONTROL_SetHigh();
-    __delay_ms(RELE_GAP);
+    __delay_ms(RELE_GAP * 1000);
     PIN_RELE_POWER_SetHigh();
-    time_pow = RELE_TIME;
+    time_pow_s = RELE_TIME;
     FLAGS.bits.RELE_POW_WAIT = 1;
     FLAGS.bits.RELE_CON_WAIT = 1;
     return;
 }
 
-void go_open() {
+void go_open() {//?????? ???????? ??????
     PIN_RELE_CONTROL_SetLow();
     PIN_RELE_POWER_SetHigh();
-    time_pow = RELE_TIME;
+    time_pow_s = RELE_TIME;
     FLAGS.bits.RELE_POW_WAIT = 1;
     return;
 }
 
-void go_close_alt() {
+void go_close_alt() {//???????? ?????? 2 ?????
     FLAGS.bits.CLOSED = 1;
     PIN_RELE_CONTROL_SetLow();
     PIN_RELE_POWER_SetHigh();
 }
 
-void go_open_alt() {
+void go_open_alt() {//???????? ?????? 2 ?????
     FLAGS.bits.CLOSED = 0;
     PIN_RELE_CONTROL_SetLow();
     PIN_RELE_POWER_SetLow();
     return;
 }
 
-start_alarm() {
+start_alarm() {//?????????? ????????
     FLAGS.bits.ALARM = 1;
     PIN_ALARM_STATE_SetHigh();
-    INTCONbits.TMR0IE = 1; //start zummer
+    INTCONbits.TMR0IE = 1; //??? ??????
     if (FLAGS.bits.NORMAL_WORK_MODE) {
         go_close();
     } else {
@@ -119,7 +119,8 @@ start_alarm() {
     }
 }
 
-void get_measure() {
+void get_measure() {//????????? ????????? ????????
+    static unsigned char measures;
     PIN_POWER_MEAS_SetHigh();
     PIN_WSP_STATE_SetAnalogMode();
     unsigned res = ADC_GetConversion(PIN_WSP_STATE);
@@ -131,7 +132,9 @@ void get_measure() {
     return;
 }
 
-void get_fun() {
+void get_fun() {//??????????? ????????? ????????????? (??????????? 1 ???)
+
+    static signed char fun_counter;
     PIN_POWER_MEAS_SetHigh();
     PIN_FUN_STATE_SetAnalogMode();
     unsigned res = ADC_GetConversion(PIN_FUN_STATE);
@@ -142,15 +145,17 @@ void get_fun() {
 
     if (fun_counter > FUN_MEAS_COUNT) {
         fun_counter = FUN_MEAS_COUNT;
-        FLAGS.bits.FUN_CONNECTED = 0;
+        FLAGS.bits._FUN_CONNECTED = 0;
     } else if (fun_counter<-FUN_MEAS_COUNT) {
         fun_counter = -FUN_MEAS_COUNT;
-        FLAGS.bits.FUN_CONNECTED = 1;
+        FLAGS.bits._FUN_CONNECTED = 1;
     }
     return;
 }
 
-void get_fun_full() {
+void get_fun_full() {//??????????? ????????? ????????????? (??????????? ??? ????)
+
+    static signed char fun_counter;
     PIN_POWER_MEAS_SetHigh();
     PIN_FUN_STATE_SetAnalogMode();
     char flag = 0;
@@ -160,11 +165,11 @@ void get_fun_full() {
         else fun_counter++;
         if (fun_counter > FUN_MEAS_COUNT) {
             fun_counter = FUN_MEAS_COUNT;
-            FLAGS.bits.FUN_CONNECTED = 0;
+            FLAGS.bits._FUN_CONNECTED = 0;
             flag = 1;
         } else if (fun_counter<-FUN_MEAS_COUNT) {
             fun_counter = -FUN_MEAS_COUNT;
-            FLAGS.bits.FUN_CONNECTED = 1;
+            FLAGS.bits._FUN_CONNECTED = 1;
             flag = 1;
         }
     } while (flag == 0);
@@ -174,61 +179,63 @@ void get_fun_full() {
     return;
 }
 
-void get_jump() {
+void get_jump() {//??????????? ????????? ???????? (??????????? 1 ???)
+
+    static signed char jump_counter;
     PIN_JUMP_STATE_SetAnalogMode();
-   unsigned res = ADC_GetConversion(PIN_JUMP_STATE);
+    unsigned res = ADC_GetConversion(PIN_JUMP_STATE);
     PIN_JUMP_STATE_SetDigitalMode();
-    
+
 
     if (res < LOW_PIN_VOLTAGE) jump_counter--;
     else jump_counter++;
 
     if (jump_counter > JUMP_MEAS_COUNT) {
         jump_counter = JUMP_MEAS_COUNT;
-        FLAGS.bits.JUMP = 1;
+        FLAGS.bits._JUMP = 1;
     } else if (jump_counter<-JUMP_MEAS_COUNT) {
         jump_counter = -JUMP_MEAS_COUNT;
-        FLAGS.bits.JUMP = 0;
+        FLAGS.bits._JUMP = 0;
     }
     return;
 }
 
-void get_jump_full() {
+void get_jump_full() {//??????????? ????????? ????????????? (??????????? ??? ????)
+
+    static signed char jump_counter;
     PIN_JUMP_STATE_SetAnalogMode();
     char flag = 0;
     do {
-       unsigned res = ADC_GetConversion(PIN_JUMP_STATE);
-      
-
+        unsigned res = ADC_GetConversion(PIN_JUMP_STATE);
         if (res < LOW_PIN_VOLTAGE) jump_counter--;
         else jump_counter++;
 
         if (jump_counter > JUMP_MEAS_COUNT) {
             jump_counter = JUMP_MEAS_COUNT;
-            FLAGS.bits.JUMP = 1;
+            FLAGS.bits._JUMP = 1;
             flag = 1;
         } else if (jump_counter<-JUMP_MEAS_COUNT) {
             jump_counter = -JUMP_MEAS_COUNT;
-            FLAGS.bits.JUMP = 0;
+            FLAGS.bits._JUMP = 0;
             flag = 1;
         }
     } while (flag == 0);
     PIN_JUMP_STATE_SetDigitalMode();
 }
 
-void rele_tick() {
-    if (FLAGS.bits.RELE_POW_WAIT) {
-        if (time_pow > 0) {
-            time_pow--;
+void rele_tick() {//???????? ?????? (???????? ?? ?????? ???????)
+    if (FLAGS.bits.RELE_POW_WAIT) {//???? ???????? ??????? ????
+        if (time_pow_s > 0) { //????? ?? ????????
+            time_pow_s--;
         } else {
-            if (FLAGS.bits.RELE_CON_WAIT) {//closing
+            if (FLAGS.bits.RELE_CON_WAIT) {//???? ???? ??????? ?? ???????????
                 PIN_RELE_POWER_SetLow();
-                __delay_ms(RELE_GAP);
+                __delay_ms(RELE_GAP * 1000);
                 PIN_RELE_CONTROL_SetLow();
                 FLAGS.bits.CLOSED = 1;
                 FLAGS.bits.RELE_CON_WAIT = 0;
                 FLAGS.bits.RELE_POW_WAIT = 0;
-            } else {//opening
+            } else {//???? ?? ??????? ?? ???????????
                 PIN_RELE_POWER_SetLow();
                 FLAGS.bits.CLOSED = 0;
                 FLAGS.bits.RELE_POW_WAIT = 0;
@@ -237,11 +244,11 @@ void rele_tick() {
     }
 }
 
-void sec_tick_work() {
+void sec_tick_work() {//?????? ?????????? ???????
     time_s++;
     rele_tick();
-    CLRWDT();
-    if (FLAGS.bits.ALARM) {//if alarm
+    CLRWDT(); // <2.1 ???
+    if (FLAGS.bits.ALARM) {
         PIN_LED_Toggle();
         toggle_tone();
     } else {//if not alarm
@@ -255,75 +262,67 @@ void sec_tick_work() {
     }
 }
 
-void povorot() {
+void povorot() {//???????????
     if ((time_s > ROTATION_TIME) &&
-            FLAGS.bits.CLOSED == 0 &&
-            FLAGS.bits.ALARM == 0 &&
+            !FLAGS.bits.CLOSED &&
+            !FLAGS.bits.ALARM &&
             FLAGS.bits.NORMAL_WORK_MODE
             ) {
         go_close();
     }
-    if ((time_s > ROTATION_TIME) &&
-            FLAGS.bits.CLOSED == 0 &&
+    if ((time_s > (ROTATION_TIME + RELE_TIME + RELE_GAP * 2)) && //???????? ???? ????????? ?????
+            FLAGS.bits.CLOSED &&
             FLAGS.bits.ALARM == 0 &&
             FLAGS.bits.NORMAL_WORK_MODE
             ) {
         go_open();
-        time_s = 0;
+        time_s = 0; //???????? ???????
     }
 
 }
 
-void fun_work() {
+void fun_work() {//?????? ?????????????
     {
-        if (FLAGS.bits.FUN_CONNECTED && 
+        if (FLAGS.bits._FUN_CONNECTED &&
                 !FLAGS.bits.ALARM &&
                 FLAGS.bits.CLOSED &&
                 !FLAGS.bits.RELE_POW_WAIT) {
             if (FLAGS.bits.NORMAL_WORK_MODE) go_open();
             else go_open_alt();
-            beep();
-             __delay_ms(100);
-            beep();
+            //???? ?????? ????
+            beep(500, 100, 40, 1); //_freq pause work_time count
         };
-        if (!FLAGS.bits.FUN_CONNECTED && 
-                !FLAGS.bits.CLOSED && 
+        if (!FLAGS.bits._FUN_CONNECTED &&
+                !FLAGS.bits.CLOSED &&
                 !FLAGS.bits.RELE_POW_WAIT) {
             if (FLAGS.bits.NORMAL_WORK_MODE) go_close();
             else go_close_alt();
-            beep();   
-             __delay_ms(100);
-            beep();   
-             __delay_ms(100);
-            beep();
+            //??? ?????? ?????
+            beep(500, 100, 40, 2); //_freq pause work_time count
         }
     }
 }
 
-void switch_wm() {
-    if (FLAGS.bits.JUMP) {
+void switch_wm() {//????? ?????? ??????
+    if (FLAGS.bits._JUMP) {
         if (!FLAGS.bits.NORMAL_WORK_MODE) {
             FLAGS.bits.NORMAL_WORK_MODE = 1;
             if (FLAGS.bits.CLOSED) go_close();
-            boop();
-             __delay_ms(100);
-            boop();
+            //??? ??????? ?????
+            beep(250, 100, 40, 2); //_freq pause work_time count;
         }
     } else {
         if (FLAGS.bits.NORMAL_WORK_MODE) {
             FLAGS.bits.NORMAL_WORK_MODE = 0;
             if (FLAGS.bits.CLOSED) go_close_alt();
-            boop();
-             __delay_ms(100);
-            boop();
-              __delay_ms(100);
-            boop();
+            //??? ??????? ?????
+            beep(250, 100, 40, 3); //_freq pause work_time count
         }
     }
 }
 
-void start_setup() {
-    //MCC
+void start_setup() {//????????? ?????????
+    //MCC ?????????????
     SYSTEM_Initialize(); // initialize the device
     INTERRUPT_GlobalInterruptEnable(); // Enable the Global Interrupts
     INTERRUPT_PeripheralInterruptEnable(); // Enable the Peripheral Interrupts
@@ -331,50 +330,62 @@ void start_setup() {
 
     TMR0_SetInterruptHandler(switch_zum);
     TMR2_SetInterruptHandler(sec_tick_work);
-    TMR2_StartTimer();
+    TMR2_StartTimer(); //?????? ????????? ????
 
+
+
+
+    //?????????? ?????????? ?????? ????? ?? ?????? ??????????
     PIN_WSP_STATE_SetDigitalMode();
-
-    PIN_JUMP_STATE_ResetPullup();
     PIN_JUMP_STATE_SetDigitalMode();
-    PIN_JUMP_STATE_SetDigitalInput();
-
-
-    PIN_FUN_STATE_ResetPullup();
     PIN_FUN_STATE_SetDigitalMode();
-    PIN_FUN_STATE_SetDigitalInput();
 
+    //????? ?????????
+    PIN_JUMP_STATE_ResetPullup();
+    PIN_JUMP_STATE_SetDigitalInput();
+    PIN_FUN_STATE_ResetPullup();
+    PIN_FUN_STATE_SetDigitalInput();
+    INTCONbits.TMR0IE = 0; //???? ??????
+    FLAGS.value = 0;
     PIN_RELE_POWER_SetLow();
     PIN_RELE_CONTROL_SetLow();
-
-    PIN_ALARM_STATE_SetDigitalOutput();
     PIN_ALARM_STATE_SetLow();
+    PIN_ALARM_STATE_SetDigitalOutput();
 
-
-    INTCONbits.TMR0IE = 0;
-    FLAGS.value = 0;
-
+    //???????? ???????? ??????
     get_fun_full();
     get_jump_full();
-    time_pow = 0;
+    time_pow_s = 0;
+
+    //time_s=get from eeprom
 }
 
 void main(void) {
-
+    
     start_setup();
+    
+#ifdef DEBUG
+       OPTION_REG = (uint8_t)((OPTION_REG & 0xC0) | (0xD7 & 0x3F)); 
+       TMR0 = 0x16;
+       timer0ReloadVal= 22;
+#endif
+       
+
+
     while (1) {
 
         __delay_ms(50);
-        if (FLAGS.bits.ALARM == 0) {
 
+        if (!FLAGS.bits.ALARM) {
             get_fun();
             fun_work();
-
             get_jump();
             switch_wm();
-
             povorot();
-           // switch_zum();
+#ifdef DEBUG
+            switch_zum();
+#endif
+
         };
     }
 }
